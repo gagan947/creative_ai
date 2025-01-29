f1a2650844f2ebc94987539ebcaed3455aa72f4e
snehamishra312 <snehamishra.github@gmail.com>
2025-01-29 18:57:30 +0530

    country list added

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CountryISO, NgxIntlTelInputModule, SearchCountryField } from 'ngx-intl-tel-input-gg';
+import { Country, State, City } from 'country-state-city'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [NgxIntlTelInputModule, ReactiveFormsModule, FormsModule],
+  imports: [NgxIntlTelInputModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
   SearchCountryField = SearchCountryField
   CountryISO = CountryISO
+  countries: any;
+
+  ngOnInit(): void {
+    this.countries = Country.getAllCountries()
+  }
 }
