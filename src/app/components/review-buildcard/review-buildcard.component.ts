58a0b0b0034a01120f7a16696a6214cd4100c20a
faraz78691 <mohdfaraz.github@gmail.com>
2025-02-11 16:24:46 +0530

    sdgasgsdg

--- src/app/components/review-buildcard/review-buildcard.component.ts
+++ src/app/components/review-buildcard/review-buildcard.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Feature } from '../../models/projects';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-review-buildcard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './review-buildcard.component.html',
   styleUrl: './review-buildcard.component.css'
 })
 export class ReviewBuildcardComponent {
 
+  projectsFeatures: Feature[] = [];
+  projectsData: any;
+  constructor(private fb: FormBuilder, private apiService: ApiService, private router: Router) {
+    let projectData = sessionStorage.getItem('projectData');
+    this.projectsData = JSON.parse(projectData!);
+    this.projectsFeatures = this.projectsData.selectdFeature;
+    console.log(this.projectsFeatures);
+
+  };
 }
