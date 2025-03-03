5536b09b880261a50560770470a83809d3231e18
faraz78691 <mohdfaraz.github@gmail.com>
2025-03-03 16:57:01 +0530

    fixed price of customization

--- src/app/components/plan-delivery/plan-delivery.component.ts
+++ src/app/components/plan-delivery/plan-delivery.component.ts
@@ -58,7 +58,8 @@ export class PlanDeliveryComponent {
     this.totalFeatureCost = this.featureCost;
     this.customizationCost = this.projectsData.customisationCost;
     this.customizationSecondCost = this.projectsData.customisationCost + (this.customizationCost * 12) / 100;;
-    this.customizationThirdCost = this.projectsData.customisationCost + (this.customizationCost * 36) / 100;;
+    this.customizationThirdCost = this.projectsData.customisationCost + (this.customizationCost * 36) / 100;
+    console.log( this.customizationThirdCost);
     this.totalCustomizeCost = this.customizationCost;
     // this.totalSubFeatures = this.projectsData.selectdFeature.reduce(
     //   (total: any, feature: { feature: string | any[]; }) => total + (feature.subFeaturesListWithPrice?.length || 0),
@@ -99,7 +100,7 @@ export class PlanDeliveryComponent {
         this.featureThirdCost = this.featureCost + ((this.featureCost * 36) / 100);
         this.customizationCost = this.customizationCost - (this.projectsData.customisationCost * 30) / 100;
         this.customizationSecondCost = this.customizationCost + (this.customizationCost * 12) / 100;
-        this.customizationThirdCost = this.customizationCost + (this.customizationCost * 30) / 100;
+        this.customizationThirdCost = this.customizationCost + (this.customizationCost * 36) / 100;
      
       }
     }
@@ -112,6 +113,9 @@ export class PlanDeliveryComponent {
       this.featureCost = this.projectsData.featuresCost;
       this.featureSecondCost = this.featureCost + (this.featureCost * 12) / 100;
       this.featureThirdCost = this.featureCost + (this.featureCost * 36) / 100;
+      this.customizationCost = this.projectsData.customisationCost;
+      this.customizationSecondCost = this.customizationCost  + (this.projectsData.customisationCost * 12) / 100;
+      this.customizationThirdCost = this.customizationCost  + (this.projectsData.customisationCost * 36) / 100;
 
     }
     if (this.rangeValue == '2') {
@@ -158,31 +162,6 @@ export class PlanDeliveryComponent {
       this.estimatedDate.setDate(today.getDate() + this.estimatedWeeks * 7);
     }
   }
-
-  onclickAND(event: any) {
-    event.stopPropagation()
-    this.isActiveAND = !this.isActiveAND;
-
-    if (this.isActiveAND) {
-      this.totalPrice = this.projectsData.totalCost
-    }
-
-  }
-  onclickIOS(event: any) {
-    event.stopPropagation()
-    this.isActiveIOS = !this.isActiveIOS;
-    if (this.isActiveIOS) {
-      const thirtyPercent = (this.projectsData.totalCost * 30) / 100
-    }
-  }
-  onclickWEB(event: any) {
-    event.stopPropagation()
-    this.isActiveWeb = !this.isActiveWeb
-  }
-  onclickMOb(event: any) {
-    event.stopPropagation()
-    this.isActiveMobileSite = !this.isActiveMobileSite
-  };
 
   Navigate() {
 
