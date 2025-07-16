950c886c969fcb07207c80d08e6790c7291a7554
gagan947 <gagan.ctinfotech@gmail.com>
2025-07-16 19:46:31 +0530

    content updated

--- src/app/components/landing-pages/enterprise/enterprise.component.ts
+++ src/app/components/landing-pages/enterprise/enterprise.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-enterprise',
@@ -11,5 +12,8 @@ import { HeaderComponent } from '../../shared/header/header.component';
   styleUrl: './enterprise.component.css'
 })
 export class EnterpriseComponent {
-
+  constructor(private meta: Meta, private title: Title) {
+    this.title.setTitle('Custom Enterprise Software Development Services | AI Solutions');
+    this.meta.updateTag({ name: 'description', content: 'Boost enterprise innovation with AI-driven custom software development. Build smart, scalable solutions that accelerate growth and give your business a competitive edge.' })
+  }
 }
